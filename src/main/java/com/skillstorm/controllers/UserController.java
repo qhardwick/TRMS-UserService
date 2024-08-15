4ce852d15c61d0cd99d9b48c945ac1ed5cf720bc
qhardwick <qhardwick@gmail.com>
2024-08-15 18:01:12 -0400

    view balance

--- src/main/java/com/skillstorm/controllers/UserController.java
+++ src/main/java/com/skillstorm/controllers/UserController.java
@@ -6,7 +6,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
